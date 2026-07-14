d5885d29421f10967688d0932274b67530bd8371
Norawit <norrawit45@gmail.com>
2026-07-14 16:55:27 +0700

    add to workshop

---
+++ 2a.py
@@ -0,0 +1,7 @@
+quantity = int(input("จำนวนปืนที่รับมาขาย : "))
+cost = int(input("ต้นทุนปืนที่รับมาขาย : "))
+sell = int(input("ราคาที่นำไปขายต่อ : "))
+team_members = int(input("จำนวนลูกน้องในทีมที่ไปทำงาน : "))
+
+sumstart = (quantity * cost) # อันนี้ต้นทุน 
+sumsell = (sell * quantity)
---
+++ 2b.py
@@ -0,0 +1,10 @@
+name = input("คุณชื่ออะไร")
+age = int(input("อายุเท่าไหร่"))
+height = float(input("ส่วนสูงเท่าไหร่"))
+power = int(input("พละกำลังเท่าไหร่ 1-10"))
+cash = int(input("เงินติดตัวเท่าไหร่"))
+if (power >= 5 and cash >= 1000) :
+    print(name + " ผ่าน ประธาน")
+else:
+    print(name + "ไม่ผ่าน")
+    
